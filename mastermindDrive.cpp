3e36e824557dc88a53a3b6930db3e2e9f5b4ede3
Uyen Tran <62521466+jenlcmc@users.noreply.github.com>
2021-06-17 23:38:19 -0700

    change file

--- mastermindDrive.cpp
+++ mastermindDrive.cpp
@@ -1,14 +1,10 @@
 #include "mastermindDrive.h"
+#include <iostream>
 #include <vector>
 #include <string>
-#include <iostream>
 using namespace std;
 
 /*MastermindLayout - constructor that sets the size of solution vector and assignseach element to contain"red", 
-
-    sets maxMoves to 5 and moves to 0.
-    int size - assign it to solution size
-
     Algorithm - assign color to default color red and maxmoves to 5, moves to 0
 */
 
@@ -24,10 +20,6 @@ MastermindLayout::MastermindLayout(int size) : GameSolution(size)
 }
 
 /*MastermindLayout - constructor that sets the solution vector with the content of UserGame vector (insensitive way)
-
-    sets maxMoves to 5 and moves to 0.
-    vector<string> UserGame -user input string that will be push to solution vector
-
     Algorithm - using nested for loop to to convert the vector of string UserGame to all uppercase and then put them in solution
     vec using push_back(). Also, assign maxMoves to 5 and moves to 0
 
@@ -48,20 +40,6 @@ MastermindLayout::MastermindLayout(vector<string> UserGame)
 }
 
 /*move - this function simulates one move/round of the MastermindLayout game. 
-    function needs to determine how many pegs’s colors are in the same respective column as the solution vector
-    (update gold each time), and the colors from playerMove that matches with solution 
-    but not in the same respective position(update silver each time), then update the moves counter. 
-
-    vector<string> playerMove- consists of the user’s guess which is a set of pegs with their colors
-    int gold - correct index(s) and same result(s)
-    int silver - wrong index(s) but same result(s)
-    return - The function returns 4 possible integers
-    –If it returns 0, the game is not over
-    -If it returns 1, the user won (i.e.  the amount of gold matches the width of the board)
-    –If it returns -1, the user lost (ran out of attempts)
-    –If it returns 2, then the amount of pegs in the playerMove does not match the amount in solution,so not a valid move,
-    not update the moves counter in this case
-
     Algorithm - using nested loop to loop through solution and player vector. Then determine the gold value by compare
     2 vectors and mark it using bool. Then inside the nested loop, determine for silver, increment silver 1st
     and then check if vector of playermove in outerloop same with insideloop or not and 
@@ -71,17 +49,17 @@ MastermindLayout::MastermindLayout(vector<string> UserGame)
 int MastermindLayout::MovePegs(vector<string> playerMove, int& gold, int& silver) 
 {
     gold = silver = 0;
-    bool haveresult = false;
+    bool havePeg = false;
 
     for(size_t i = 0; i < GameSolution.size(); i++){
         if(playerMove[i] == GameSolution[i]){
             gold++;
-            haveresult = true;
+            havePeg = true;
         }
         for(size_t j = 0; j < playerMove.size(); j++){
             if(playerMove[j] == GameSolution[i] && i != j){
                 silver++;
-                if(playerMove[i] == playerMove[j] && haveresult){
+                if(playerMove[i] == playerMove[j] && havePeg){
                     silver--;
                     break;
                 }
@@ -89,10 +67,7 @@ int MastermindLayout::MovePegs(vector<string> playerMove, int& gold, int& silver
         }
     }
     GameMove++;
-
-    if(playerMove.size() != GameSolution.size()){ 
-        return 2;
-    }
+    
     if(static_cast<size_t>(gold) == GameSolution.size()){
         return 1;
     }
@@ -103,8 +78,14 @@ int MastermindLayout::MovePegs(vector<string> playerMove, int& gold, int& silver
         return 0;
     }
     return 3;
+    if(playerMove.size() != GameSolution.size()){ 
+        return 2;
+    }
 }
 
+/*getSolution - get game solution
+    Algorithm - take colors from vector and cout 
+*/
 void MastermindLayout::getSolution(){
     for(auto i : GameSolution){
         cout << i << " " << endl;
@@ -112,8 +93,6 @@ void MastermindLayout::getSolution(){
 }
 
 /*getMoveIndex - get move index
-    return move + 1
-
     Algorithm - return move +1 cause UserGame is 0
 */
 int MastermindLayout::GetIndexOfMove() const
@@ -122,8 +101,6 @@ int MastermindLayout::GetIndexOfMove() const
 }
 
 /*getMoveIndex - get solution size/ columns numbers
-    return solution size
-
     Algorithm - return solution size
 */
 int MastermindLayout::GetColumns() const
@@ -131,6 +108,10 @@ int MastermindLayout::GetColumns() const
     return(GameSolution.size()); 
 }
 
+/*ScoreTracking - keep track of the score (user and machine)
+    Algorithm - if user win -> +1 for user. Same with computer
+    Make a board and cout the result after the game end
+*/
 void MastermindLayout::ScoreTracking(int& initial){
     if(initial == 1){
         UserScore = UserScore + 1;
